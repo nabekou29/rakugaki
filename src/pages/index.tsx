07f35764ce2880f8fbe8b5c2394924241e95eaf6
nabekou29 <nabekou0729@gmail.com>
2026-02-08 22:13:07 +0900

    Squashed 't3-todo-app/' content from commit 4cebdb7
    git-subtree-dir: t3-todo-app
    git-subtree-split: 4cebdb79de19e4b574610b3e045d31dd1db19c67

---
+++ src/pages/index.tsx
@@ -0,0 +1,76 @@
+import Head from "next/head";
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { CreateTodo } from "~/components/CreateTodo";
+import { Todos } from "~/components/Todos";
+
+function Home() {
+  const { data: sessionData, status } = useSession();
+  return (
+    <>
+      <Head>
+        <title>Todo App</title>
+        <meta name="description" content="Full stack todo app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="min-h-screen bg-olive-one p-0 selection:bg-green-two md:px-8 md:py-24">
+        <main className="mx-auto min-h-screen max-w-none rounded-none bg-cream-four px-5 pb-10 pt-24 outline-none md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-cream-four">
+          <h1 className="mb-6 text-center text-4xl font-bold text-gray-three">
+            ToDo List
+          </h1>
+          {status !== "loading" && sessionData && (
+            // status が "loading" でない、つまり認証情報の取得が完了している、
+            // かつ、認証されている場合に、下記が表示されます
+            <>
+              <div className="flex flex-col items-center">
+                <p className="text-l text-white mb-4 text-center">
+                  <span>Logged in as {sessionData.user?.email}</span>
+                </p>
+                <button
+                  className="mb-8 inline-flex cursor-pointer items-center justify-center rounded-md px-4 py-2 font-semibold outline outline-2 outline-offset-2 outline-green-one hover:text-green-five"
+                  onClick={() => void signOut()}
+                >
+                  Sign out
+                </button>
+              </div>
+              <div>
+                <CreateTodo />
+                <Todos />
+              </div>
+            </>
+          )}
+          {status !== "loading" && !sessionData && (
+            // status が "loading" でない、つまり認証情報の取得が完了している、
+            // かつ、認証されていない場合に、下記が表示されます
+            <div className="flex flex-col items-center">
+              <button
+                className="mb-5 inline-flex cursor-pointer items-center justify-center rounded-md px-4 py-2 font-semibold outline outline-2 outline-offset-2 outline-green-one hover:text-green-five"
+                onClick={() => void signIn()}
+              >
+                Sign In
+              </button>
+              <div className="mb-5 text-xl">
+                <p className="text-center text-gray-four">
+                  Keep your life in order with todolist
+                </p>
+                <p className="text-center text-gray-four">
+                  - The ultimate productivity tool -
+                </p>
+              </div>
+              <div className="">
+                <Image
+                  src="/images/main-img.png"
+                  width={600}
+                  height={600}
+                  alt="main-img"
+                />
+              </div>
+            </div>
+          )}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/server/api/routers/todo.ts
@@ -0,0 +1,79 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { createInput, toggleInput, updateInput } from "~/server/types";
+
+export const todoRouter = createTRPCRouter({
+  all: protectedProcedure.query(async ({ ctx }) => {
+    const todos = await ctx.prisma.todo.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return todos.map(({ id, text, isCompleted }) => ({
+      id,
+      text,
+      isCompleted,
+    }));
+  }),
+  create: protectedProcedure
+    .input(createInput)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.todo.create({
+        data: {
+          text: input,
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+  toggle: protectedProcedure
+    .input(toggleInput)
+    .mutation(async ({ ctx, input }) => {
+      const { id, is_completed } = input;
+
+      return ctx.prisma.todo.update({
+        where: {
+          id,
+        },
+        data: {
+          isCompleted: is_completed,
+        },
+      });
+    }),
+
+  update: protectedProcedure
+    .input(updateInput)
+    .mutation(async ({ ctx, input }) => {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "failed to update todo",
+      });
+      const { id, text } = input;
+
+      return ctx.prisma.todo.update({
+        where: {
+          id,
+        },
+        data: {
+          text,
+        },
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.todo.delete({
+        where: {
+          id: input,
+        },
+      });
+    }),
+});
